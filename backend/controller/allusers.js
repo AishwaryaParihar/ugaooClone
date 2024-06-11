e28b5be5ed6c4e259f1f2930214a5ceb49e617b4
aish <toshiparihar121212@gmail.com>
2024-06-11 10:37:05 +0530

    add two api 'all users' and 'update users'

--- backend/controller/allusers.js
+++ backend/controller/allusers.js
@@ -1,6 +1,17 @@
+const userModel = require("../models/userModel")
+
 async function allUsers(req, res){
     try{
         console.log('userid all users',req.userId)
+
+        const allUsers = await userModel.find()
+
+        res.json({
+            message : "All User",
+            data : allUsers,
+            success : true,
+            error : false
+        })
     }
     catch(err){
         res.status(400).json({
